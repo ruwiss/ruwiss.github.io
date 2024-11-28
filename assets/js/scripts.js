a3cec2e1227496bc55234390ed0c5f049b3bf054
ruwiss <bihinmehen@gmail.com>
2024-11-28 16:36:37 +0300

    omergundgr

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,3 +40,15 @@ document.addEventListener("DOMContentLoaded", function () {
     navRight.classList.toggle("active");
   });
 });
+
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/67486a514304e3196ae9dc39/1idpdujr3";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
